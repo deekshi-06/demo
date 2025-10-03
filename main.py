ee5c5cdd942b2445a127511689a3401e00a80f07
Deekshitha K <deekshitha.kbhat@gmail.com>
2025-10-03 06:12:50 +0000

    fix: bug in radius calculation

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ def calculate_circumference(radius):
 def calculate_diameter(radius):
     return 2 * radius
 
+def calculate_radius(diameter):
+    return diameter / 2
+
 if __name__ == "__main__":
     r = 5
     area = calculate_area(r)
     circumference = calculate_circumference(r)
     diameter = calculate_diameter(r)
+    radius = calculate_radius(diameter)
     print(f"Area: {area}")
     print(f"Circumference: {circumference}")
     print(f"Diameter: {diameter}")
+    print(f"Radius: {radius}")
